8f12de72ca6c59e9dd14e0adff043dc6b9fef966
T.NITISH <mbeastreacts471@gmail.com>
2026-07-14 10:28:29 +0530

    Introduction to Java

--- Day01/main.java
+++ Day01/main.java
@@ -1,5 +1,22 @@
-package Day01;
+//example 1:
+public class Main{
+    public static void main(String[]args){
+        //program 1
+        System.out.println("Hello,World");
 
-public class main {
-    
+        //program 2
+        System.out.println("My name is Nitish");
+        System.out.println("I study CSE AI & ML");
+        System.out.println("I am learning Java");
+
+        //program 3
+        System.out.print("Hello");
+        System.out.println("Java!");
+
+        //program 4
+        System.out.println("******");
+        System.out.println("******");
+        System.out.println("******");
+    }
 }
+
