86f04f5a8dd92d7a7c9b5f810739637775af0e46
T.NITISH <mbeastreacts471@gmail.com>
2026-07-15 11:34:06 +0530

    Program-1

---
+++ Day03:Operators/Main.java
@@ -0,0 +1,29 @@
+
+/*
+==========================================
+        DAY 03 - OPERATORS
+==========================================
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // =========================================
+        // Program 1 - Arithmetic Operators
+        // =========================================
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println("========== ARITHMETIC OPERATORS ==========");
+        System.out.println();
+
+        System.out.println("Addition       : " + (a + b));
+        System.out.println("Subtraction    : " + (a - b));
+        System.out.println("Multiplication : " + (a * b));
+        System.out.println("Division       : " + (a / b));
+        System.out.println("Modulus        : " + (a % b));
+
+    }
+}
