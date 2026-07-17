249c1d0803f403a176810cb2e92a5668ae9b097b
T.NITISH <mbeastreacts471@gmail.com>
2026-07-17 12:24:42 +0530

    program-3

--- Day05:Conditional Statements/Main.java
+++ Day05:Conditional Statements/Main.java
@@ -45,6 +45,23 @@ public class Main {
       }else{
         System.out.println("Fail");
       }
+      //program-3:
+
+      if(marks>=90){
+        System.out.println("Grade:S");
+      }else if(marks>=80){
+        System.out.println("Grade:A");
+      }else if(marks>=70){
+        System.out.println("Grade:B");
+      }else if(marks>=60){
+        System.out.println("Grade:C");
+      }else if(marks>=50){
+        System.out.println("Grade:D");
+      }else if(marks>=40){
+        System.out.println("Grade:E");
+      }else{
+        System.out.println("Grade:F");
+      }
         input.close();
     }
 }
