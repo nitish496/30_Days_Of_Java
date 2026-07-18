dd8d07458fc094f564bc221c795881e2ab5a3255
T.NITISH <mbeastreacts471@gmail.com>
2026-07-18 16:55:42 +0530

    Day:07:pattern printing

---
+++ Day07: Pattern Printing/Main.java
@@ -0,0 +1,14 @@
+//program-1:right star program
+public class Main{
+public static void main(String[] args) {
+    
+
+
+for(int row =1;row<=5;row++){
+    for(int column = 1;column<=row;column++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+ } 
+}
