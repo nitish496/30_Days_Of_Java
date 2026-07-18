0a9c04d1e69ee1b8ee3087d68377d5b707465bf1
T.NITISH <mbeastreacts471@gmail.com>
2026-07-18 21:49:56 +0530

    program-1 for day:08

---
+++ Day08:Array/Main.java
@@ -0,0 +1,39 @@
+/*
+==========================================
+        DAY 08 - ARRAYS
+==========================================
+
+Program 1 - Create and Print an Array
+
+Author: Nitish
+==========================================
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // =========================================
+        // Program 1 - Create and Print an Array
+        // =========================================
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First Element  : " + numbers[0]);
+        System.out.println("Second Element : " + numbers[1]);
+        System.out.println("Third Element  : " + numbers[2]);
+        System.out.println("Fourth Element : " + numbers[3]);
+        System.out.println("Fifth Element  : " + numbers[4]);
+
+    }
+}
+
+/*
+==========================================
+End of Program 1
+
+Next Program:
+Take Array Input from User
+
+==========================================
+*/
