aa8e399cd22df683d156225e695e2e91592f6b14
T.NITISH <mbeastreacts471@gmail.com>
2026-07-18 22:18:29 +0530

    program-3 for day:08

--- Day08:Array/Main.java
+++ Day08:Array/Main.java
@@ -24,11 +24,40 @@ public class Main {
         System.out.println("Third Element  : " + numbers[2]);
         System.out.println("Fourth Element : " + numbers[3]);
         System.out.println("Fifth Element  : " + numbers[4]);
+        System.out.println();
        //program-2:
        int [] arr= {10,20,30,40,50};
        for(int i=0;i<arr.length;i++){
         System.out.println(arr[i]);
        }
+       System.out.println();
+       //program-3:
+       int []arri = {10,20,30,40,50};
+       int sum =0;
+       for(int j=0;j<arri.length;j++){
+        sum +=arri[j];
+       }
+       System.out.println("Sum:"+sum);
+       System.out.println();
+       //program-4:
+       int summ =0;
+      int arrn[]={10,20,30,40,50};
+      for(int k=0;k<arr.length;k++){
+        summ +=arr[k];
+      }
+      double avg = (double) summ /arrn.length;
+      System.out.println("Average:"+avg);
+      System.out.println();
+
+      //program-5
+      int[]nums = {10,20,30,40,50};
+      int max = nums[0];
+      for(int f=0;f<nums.length;f++){
+        if(nums[f]>max){
+            max = nums[f];
+        }
+      }
+      System.out.println("MAX:"+max);
     }
 }
 
