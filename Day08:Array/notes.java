6770b29309da29643a3daff9d780fc0e80dfd791
T.NITISH <mbeastreacts471@gmail.com>
2026-07-18 21:47:22 +0530

    notes for day:07

---
+++ Day08:Array/notes.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int[]arr = {10,20,30,40,50};
+        System.out.println("first element:"+arr[0]);
+          System.out.println("second element:"+arr[1]);
+            System.out.println("third element:"+arr[2]);
+              System.out.println("fourth element:"+arr[3]);
+                System.out.println("fifth element:"+arr[4]);
+    }
+}
