93dcaf6d2cb1265bc59c8d61185547bf6bed4d77
T.NITISH <mbeastreacts471@gmail.com>
2026-07-18 18:41:57 +0530

    refined code for day-7

--- Day07: Pattern Printing/Main.java
+++ Day07: Pattern Printing/Main.java
@@ -1,89 +1,202 @@
-//program-1:right star program
-public class Main{
-public static void main(String[] args) {
-    
+/*
+==========================================
+        DAY 07 - PATTERN PRINTING
+==========================================
+
+Topics Covered:
+1. Right Star Triangle
+2. Inverted Star Triangle
+3. Number Triangle
+4. Inverted Number Triangle
+5. Repeated Number Pattern
+6. Floyd's Triangle
+7. Character Pattern
+8. Hollow Square
+9. Pyramid Pattern
+
+Author: Nitish
+==========================================
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // =========================================
+        // Program 1 - Right Star Triangle
+        // =========================================
+
+        System.out.println("========== RIGHT STAR TRIANGLE ==========\n");
+
+        for (int row = 1; row <= 5; row++) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
 
 
-for(int row =1;row<=5;row++){
-    for(int column = 1;column<=row;column++){
-        System.out.print("*");
+        // =========================================
+        // Program 2 - Inverted Star Triangle
+        // =========================================
+
+        System.out.println("========== INVERTED STAR TRIANGLE ==========\n");
+
+        for (int row = 5; row >= 1; row--) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 3 - Number Triangle
+        // =========================================
+
+        System.out.println("========== NUMBER TRIANGLE ==========\n");
+
+        for (int row = 1; row <= 5; row++) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 4 - Inverted Number Triangle
+        // =========================================
+
+        System.out.println("========== INVERTED NUMBER TRIANGLE ==========\n");
+
+        for (int row = 5; row >= 1; row--) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 5 - Repeated Number Pattern
+        // =========================================
+
+        System.out.println("========== REPEATED NUMBER PATTERN ==========\n");
+
+        for (int row = 1; row <= 5; row++) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 6 - Floyd's Triangle
+        // =========================================
+
+        System.out.println("========== FLOYD'S TRIANGLE ==========\n");
+
+        int number = 1;
+
+        for (int row = 1; row <= 5; row++) {
+            for (int column = 1; column <= row; column++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 7 - Character Pattern
+        // =========================================
+
+        System.out.println("========== CHARACTER PATTERN ==========\n");
+
+        for (int row = 1; row <= 5; row++) {
+
+            char alphabet = 'A';
+
+            for (int column = 1; column <= row; column++) {
+                System.out.print(alphabet + " ");
+                alphabet++;
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 8 - Hollow Square
+        // =========================================
+
+        System.out.println("========== HOLLOW SQUARE ==========\n");
+
+        for (int row = 1; row <= 5; row++) {
+
+            for (int column = 1; column <= 5; column++) {
+
+                if (row == 1 || row == 5 || column == 1 || column == 5) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        // =========================================
+        // Program 9 - Pyramid Pattern
+        // =========================================
+
+        System.out.println("========== PYRAMID PATTERN ==========\n");
+
+        for (int row = 1; row <= 5; row++) {
+
+            // Print Spaces
+            for (int space = 1; space <= 5 - row; space++) {
+                System.out.print(" ");
+            }
+
+            // Print Stars
+            for (int star = 1; star <= (2 * row - 1); star++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
     }
-    System.out.println();
 }
-  System.out.println();
-    //program-2:inverted star triangle
-    for(int i =5; i>=1;i--){
-        for(int j =1;j<=i;j++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-    System.out.println();
 
-    //program-3:Number triangle
-    for(int q=1;q<=5;q++){
-        for(int w=1;w<=q;w++){
-            System.out.print(w+" ");
-        }
-        System.out.println();
-    }
-    System.out.println();
+/*
+==========================================
+End of Day 07
 
-    //program-4:Inverted number triangle
-     for(int z=5;z>=1;z--){
-        for(int x =1;x<=z;x++){
-            System.out.print(x+" ");
-        }
-        System.out.println();
-    }
-    System.out.println();
+Next Topic:
+Arrays
 
-    //program-5:repeated number pattern
-    for(int m=1;m<=5;m++){
-        for(int n=1;n<=m;n++){
-            System.out.print(m+" ");
-        }
-        System.out.println();
-    }
-    System.out.println();
-    //program-6:
-    int num=1;
-    for(int v=1;v<=5;v++){
-        for(int b=1;b<=v;b++){
-            System.out.print(num+" ");
-            num++;
-        }
-        System.out.println();
-    }
-    System.out.println();
-  
-
-   for (int c = 1; c <= 5; c++) {
-
-    char alphabet = 'A';
-
-    for (int v = 1; v <= c; v++) {
-        System.out.print(alphabet + " ");
-        alphabet++;
-    }
-
-    System.out.println();
-}
-
-System.out.println();
-   
-
-//program-8:
-for(int a =1;a<=5;a++){
-    for(int s=1;s<=5;s++){
-        if(a==1||a== 5||s==1||s==5){
-            System.out.print("*");
-        }else{
-            System.out.print(" ");
-        }
-    }
-    System.out.println();
-}
-System.out.println();
- } 
-}
+==========================================
+*/
