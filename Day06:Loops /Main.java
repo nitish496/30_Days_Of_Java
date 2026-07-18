e40cc1fc1a2a83f558c2e2131b97f7d707e3bb84
T.NITISH <mbeastreacts471@gmail.com>
2026-07-18 11:03:52 +0530

    program-2 for day:06

--- Day06:Loops /Main.java
+++ Day06:Loops /Main.java
@@ -3,5 +3,13 @@ public class Main{
         for(int i=0;i<5;i++){
             System.out.println("Hello");
         }
+        System.out.println();
+        //program-2
+        int i=1;
+        while(i<=5){
+            System.out.println("Hello");
+            i++;
+        }
+        System.out.println();
     }
 }
