f8fe20766794108e84eb91c0f69d49d5af9560b3
T.NITISH <mbeastreacts471@gmail.com>
2026-07-19 17:48:15 +0530

    refined notes for day-09

--- Day09:Methods/Main.java
+++ Day09:Methods/Main.java
@@ -1,84 +1,206 @@
-//program-1:
-public class Main{
-    public static void printHello(){
-        System.out.println("Hello,Java");
-    }
-    //program-2:
-    public static void printMessage(){
-        System.out.println("Welcome to java");
+/*
+==========================================
+        DAY 09 - METHODS
+==========================================
+
+Author: Nitish
+==========================================
+*/
+
+public class Main {
+
+    // =========================================
+    // Program 1 - Print Hello Using Method
+    // =========================================
+
+    public static void printHello() {
+        System.out.println("Hello, Java");
     }
 
-    //program-3:
-    public static void greet(String name){
-        System.out.println("Hello, "+name+" !");
+    // =========================================
+    // Program 2 - Call Method Multiple Times
+    // =========================================
+
+    public static void printMessage() {
+        System.out.println("Welcome to Java");
     }
-    //program-4:
-    public static void addNumbers(int num1,int num2){
-        System.out.println(num1+num2);
+
+    // =========================================
+    // Program 3 - Method with Parameters
+    // =========================================
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
-    //program-5:
-    public static void squareNum(int num3){
-        System.out.println(num3*num3);
+
+    // =========================================
+    // Program 4 - Add Two Numbers Using Method
+    // =========================================
+
+    public static void addNumbers(int firstNumber, int secondNumber) {
+        System.out.println("Sum = " + (firstNumber + secondNumber));
     }
-    //program-6:
-    public static void MaxNum(int num4,int num5){
-        if(num4>num5){
-            System.out.println("Maximum number is :"+num4);
-        }else if (num5>num4){
-            System.out.println("Maximum number is:"+num5);
-        }else{
-            System.out.println("Both are equal");
+
+    // =========================================
+    // Program 5 - Find Square of a Number
+    // =========================================
+
+    public static void squareNumber(int number) {
+        System.out.println("Square = " + (number * number));
+    }
+
+    // =========================================
+    // Program 6 - Find Maximum of Two Numbers
+    // =========================================
+
+    public static void findMaximum(int firstNumber, int secondNumber) {
+
+        if (firstNumber > secondNumber) {
+            System.out.println("Maximum Number = " + firstNumber);
+        } else if (secondNumber > firstNumber) {
+            System.out.println("Maximum Number = " + secondNumber);
+        } else {
+            System.out.println("Both Numbers are Equal");
         }
+
     }
-    //program-7:
-    public static void EvenOdd(int num6){
-        if(num6%2==0){
-            System.out.println("Even number");
-        }else{
-            System.out.println("Odd number");
+
+    // =========================================
+    // Program 7 - Check Even or Odd
+    // =========================================
+
+    public static void checkEvenOdd(int number) {
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even Number");
+        } else {
+            System.out.println(number + " is an Odd Number");
         }
+
     }
-    //program-8:
-    public static int addNum(int num7,int num8){
-        int sum = num7+num8;
+
+    // =========================================
+    // Program 8 - Method Returning a Value
+    // =========================================
+
+    public static int addNumbers(int firstNumber, int secondNumber) {
+
+        int sum = firstNumber + secondNumber;
+
         return sum;
-    }
-    //program-9:
-    public static int findfactorial(int num9){
-        int factorial=1;
-        for(int index=1;index<=num9;index++){
-            factorial *=index;
 
-        }
-        return factorial;
     }
-        public static void main(String[] args) {
+
+    // =========================================
+    // Program 9 - Find Factorial Using Method
+    // =========================================
+
+    public static int findFactorial(int number) {
+
+        int factorial = 1;
+
+        for (int index = 1; index <= number; index++) {
+            factorial *= index;
+        }
+
+        return factorial;
+
+    }
+
+    public static void main(String[] args) {
+
+        // =========================================
+        // Program 1 - Print Hello Using Method
+        // =========================================
+
         printHello();
+
         System.out.println();
+
+        // =========================================
+        // Program 2 - Call Method Multiple Times
+        // =========================================
+
         printMessage();
         printMessage();
         printMessage();
         printMessage();
         printMessage();
+
         System.out.println();
-        greet("nitish");
-        greet("ram");
-        greet("king");
+
+        // =========================================
+        // Program 3 - Method with Parameters
+        // =========================================
+
+        greet("Nitish");
+        greet("Ram");
+        greet("King");
+
         System.out.println();
-        addNumbers(20,30);
+
+        // =========================================
+        // Program 4 - Add Two Numbers Using Method
+        // =========================================
+
+        addNumbers(20, 30);
         addNumbers(50, 60);
+
         System.out.println();
-        squareNum(5);
+
+        // =========================================
+        // Program 5 - Find Square of a Number
+        // =========================================
+
+        squareNumber(5);
+
         System.out.println();
-        MaxNum(10, 20);
-        MaxNum(10,10);
+
+        // =========================================
+        // Program 6 - Find Maximum of Two Numbers
+        // =========================================
+
+        findMaximum(10, 20);
+        findMaximum(10, 10);
+
         System.out.println();
-        EvenOdd(10);
+
+        // =========================================
+        // Program 7 - Check Even or Odd
+        // =========================================
+
+        checkEvenOdd(10);
+        checkEvenOdd(15);
+
         System.out.println();
-        int result1 = addNum(10, 20);
-        System.out.println("Sum = "+result1);
+
+        // =========================================
+        // Program 8 - Method Returning a Value
+        // =========================================
+
+        int result = addNumbers(10, 20);
+
+        System.out.println("Sum = " + result);
+
         System.out.println();
-        int result = findfactorial(5);
-        System.out.println("result:"+result);
+
+        // =========================================
+        // Program 9 - Find Factorial Using Method
+        // =========================================
+
+        int factorial = findFactorial(5);
+
+        System.out.println("Factorial = " + factorial);
+
     }
 }
+
+/*
+==========================================
+End of Day 09
+
+Next Topic:
+Strings
+
+==========================================
+*/
