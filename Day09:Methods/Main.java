55ee69f8d07fa185867e37475f2788de9bd07ae9
T.NITISH <mbeastreacts471@gmail.com>
2026-07-19 16:34:08 +0530

    program-2 for day:09

--- Day09:Methods/Main.java
+++ Day09:Methods/Main.java
@@ -1,9 +1,21 @@
 //program-1:
-public class Main {
-    public static void printHello() {
-         System.out.println("Hello,Java!");
+public class Main{
+    public static void printHello(){
+        System.out.println("Hello,Java");
     }
-    public static void main (String[]args){
+
+
+    //program-2:
+    public static void printMessage(){
+        System.out.println("Welcome to java");
+    }
+    public static void main(String[] args) {
         printHello();
+        System.out.println();
+        printMessage();
+        printMessage();
+        printMessage();
+        printMessage();
+        printMessage();
     }
 }
