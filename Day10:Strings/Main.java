465578ddfdb95c592e8f9cea7775d723c33999d6
T.NITISH <mbeastreacts471@gmail.com>
2026-07-19 23:30:22 +0530

    program-1 of day-10

---
+++ Day10:Strings/Main.java
@@ -0,0 +1,33 @@
+/*
+==========================================
+        DAY 10 - STRINGS
+==========================================
+
+Author: Nitish
+==========================================
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // =========================================
+        // Program 1 - Create and Print a String
+        // =========================================
+
+        String name = "Nitish";
+
+        System.out.println(name);
+
+    }
+}
+
+/*
+==========================================
+End of Day 10
+
+Next Program:
+Take String Input
+
+==========================================
+*/
