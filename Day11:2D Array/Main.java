80eb100557d3e45dfdf94a157fe6fd98c2c8a196
T.NITISH <mbeastreacts471@gmail.com>
2026-07-21 09:34:05 +0530

    program-4 for day-11

--- Day11:2D Array/Main.java
+++ Day11:2D Array/Main.java
@@ -36,6 +36,16 @@ public class Main {
             System.out.print(nums[ro][co]+" ");
         }
         System.out.println();
-     }
+    }
+        //p-4:
+        int[][]nu={{10,20,30},{40,50,60},{70,80,90}};
+        int sum =0;
+        for(int w=0;w<nu.length;w++){
+            for(int q=0;q<nu[w].length;q++){
+                sum+=nu[w][q];
+            }
+        }
+        System.out.println("Sum of all elements:"+sum);
+     
     }
 }
