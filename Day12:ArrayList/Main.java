209533452ac72d1aa61daf091cf51a3170a317d1
T.NITISH <mbeastreacts471@gmail.com>
2026-07-21 22:25:56 +0530

    program-2 for day-12

--- Day12:ArrayList/Main.java
+++ Day12:ArrayList/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 public class Main {
    public static void main(String[] args) {
+    //p-1:
     ArrayList<Integer>numbers= new ArrayList<>();
     numbers.add(10);
      numbers.add(20);
@@ -9,5 +11,32 @@ public class Main {
 
        System.out.print("ArrayList Elements:");
        System.out.println(numbers);
+
+// =========================================
+// Program 2 - Take Input into an ArrayList
+// =========================================
+
+Scanner sc = new Scanner(System.in);
+
+ArrayList<Integer> num = new ArrayList<>();
+
+System.out.print("How many elements do you want to enter?: ");
+int size = sc.nextInt();
+
+System.out.println("Enter the values:");
+
+for (int i = 0; i < size; i++) {
+
+    int number = sc.nextInt();
+
+    num.add(number);
+
+}
+
+System.out.println("ArrayList:");
+System.out.println(num);
+       sc.close();
+
+
    } 
 }
