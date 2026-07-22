ed30683b8f7b7c803a6e9474581f142f669db8a7
T.NITISH <mbeastreacts471@gmail.com>
2026-07-22 16:58:46 +0530

    program-1 for day-13

---
+++ Day13:Wrapper Classes & Autoboxing/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //p-1:
+        int number = 100;
+        Integer wrapperNumber = 100;
+
+        System.out.println("primirtive value:"+number);
+        System.out.println("Wrapper value:"+wrapperNumber);
+
+
+
+
+    }
+}
