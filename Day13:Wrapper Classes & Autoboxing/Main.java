99095fa9d7270b5f39e31af53989ca5b07bd567b
T.NITISH <mbeastreacts471@gmail.com>
2026-07-22 16:59:07 +0530

    program-2 for day-13

--- Day13:Wrapper Classes & Autoboxing/Main.java
+++ Day13:Wrapper Classes & Autoboxing/Main.java
@@ -7,7 +7,25 @@ public class Main {
         System.out.println("primirtive value:"+number);
         System.out.println("Wrapper value:"+wrapperNumber);
 
+            //p-2:
+        //primitive values:
+        int age = 18;
+        String name="Nitish";
+        double num = 35000.5;
+        char grade = 'A';
+       //Wrapper objects:
+       Integer wrapperAge=18;
+       Double  wrapperSalary = 35000.5;
+       Character wrapperGrade = 'A';
 
+       System.out.println("Primitive types:");
+       System.out.println("age:"+age);
+       System.out.println("num:"+num);
+       System.out.println("grade:"+grade);
+        System.out.println("Wrapper objects:");
+        System.out.println("age:"+wrapperAge);
+        System.out.println("num:"+wrapperNumber);
+       System.out.println("grade:"+wrapperGrade);
 
 
     }
