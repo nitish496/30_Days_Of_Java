a0af21f54c2f40e25cc7cb994b5e9624070297e6
T.NITISH <mbeastreacts471@gmail.com>
2026-07-23 10:12:26 +0530

    program-2  for day-14

--- Day14:ObjectOrientedProgramming/Main.java
+++ Day14:ObjectOrientedProgramming/Main.java
@@ -27,6 +27,47 @@ public class Main {
         System.out.println("- Inheritance");
         System.out.println("- Polymorphism");
         System.out.println("- Abstraction");
+        //p-2:
+         System.out.println("===== Objects in Real Life =====\n");
+
+        // Example 1: Car
+        System.out.println("Object: Car");
+        System.out.println("State:");
+        System.out.println("- Brand : BMW");
+        System.out.println("- Color : Black");
+        System.out.println("- Speed : 120 km/h");
+
+        System.out.println("\nBehavior:");
+        System.out.println("- Start()");
+        System.out.println("- Accelerate()");
+        System.out.println("- Brake()");
+        System.out.println("- Stop()");
+
+        System.out.println("\nIdentity:");
+        System.out.println("- Registration Number: MH12AB1234");
+
+        System.out.println("\n----------------------------------\n");
+
+        // Example 2: Student
+        System.out.println("Object: Student");
+        System.out.println("State:");
+        System.out.println("- Name : Nitish");
+        System.out.println("- Age  : 20");
+        System.out.println("- Course : B.Tech");
+
+        System.out.println("\nBehavior:");
+        System.out.println("- Study()");
+        System.out.println("- AttendClass()");
+        System.out.println("- WriteExam()");
+
+        System.out.println("\nIdentity:");
+        System.out.println("- Roll Number: 101");
+
+        System.out.println("\n==================================");
+        System.out.println("Every object has:");
+        System.out.println("1. State (Data)");
+        System.out.println("2. Behavior (Actions)");
+        System.out.println("3. Identity (Unique Identification)");
 
     }
 }
