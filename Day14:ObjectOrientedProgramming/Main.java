bf067e04d4bbc5b627c238fa141c8fdbb0894557
T.NITISH <mbeastreacts471@gmail.com>
2026-07-23 10:11:01 +0530

    program-1  for day-14

---
+++ Day14:ObjectOrientedProgramming/Main.java
@@ -0,0 +1,32 @@
+// =========================================
+// Program 1 - Introduction to OOP
+// =========================================
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Object-Oriented Programming (OOP) =====");
+
+        System.out.println("OOP is a programming paradigm that organizes programs using objects.");
+        System.out.println();
+
+        System.out.println("Advantages of OOP:");
+        System.out.println("1. Code Reusability");
+        System.out.println("2. Better Organization");
+        System.out.println("3. Easy Maintenance");
+        System.out.println("4. Security");
+        System.out.println("5. Scalability");
+
+        System.out.println();
+
+        System.out.println("Java uses OOP concepts like:");
+        System.out.println("- Classes");
+        System.out.println("- Objects");
+        System.out.println("- Encapsulation");
+        System.out.println("- Inheritance");
+        System.out.println("- Polymorphism");
+        System.out.println("- Abstraction");
+
+    }
+}
