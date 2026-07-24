4ca093ad768843cc534e310272bdcea69439f1dc
T.NITISH <mbeastreacts471@gmail.com>
2026-07-24 10:58:38 +0530

    program-3 for day-15

--- Day15:Classes & Objects/Main.java
+++ Day15:Classes & Objects/Main.java
@@ -11,7 +11,7 @@ class Student {
 
     // Method
     void study() {
-        System.out.println("Student is studying Java.");
+        System.out.println(name+" is studying Java.");
     }
 }
 
@@ -21,16 +21,43 @@ public class Main {
 
         System.out.println("Student class created successfully.");
 
+        //p-2 creating an object
         Student student1 = new Student();
         student1.name="Raju";
         student1.age=23;
         student1.course="CSE-CORE";
 
-       System.out.println("Student Details");
+        //p-3:creating different objects at a time
+         Student student2 = new Student();
+        student2.name="Aman";
+        student2.age=18;
+        student2.course="CSE-AI-ML";
+        
+        Student student3 = new Student();
+        student3.name="daniel";
+        student3.age=19;
+        student3.course="CSE-BLOCKCHAIN";
+       System.out.println("Student 1 Details");
         System.out.println("Name   : " + student1.name);
         System.out.println("Age    : " + student1.age);
         System.out.println("Course : " + student1.course);
         student1.study();
+        System.out.println();
+
+        System.out.println("Student 2 Details");
+        System.out.println("Name   : " + student2.name);
+        System.out.println("Age    : " + student2.age);
+        System.out.println("Course : " + student2.course);
+        student2.study();
+        System.out.println();
+
+        System.out.println("Student 3 Details");
+        System.out.println("Name   : " + student3.name);
+        System.out.println("Age    : " + student3.age);
+        System.out.println("Course : " + student3.course);
+        student3.study();
+        System.out.println();
+
 
 
     }
