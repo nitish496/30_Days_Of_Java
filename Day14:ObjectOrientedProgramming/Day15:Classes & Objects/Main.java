fb95898d7a1c284440ef26a97037c944fd00464d
T.NITISH <mbeastreacts471@gmail.com>
2026-07-24 10:39:19 +0530

    program-1 for day-15

---
+++ Day14:ObjectOrientedProgramming/Day15:Classes & Objects/Main.java
@@ -0,0 +1,16 @@
+//p-1:
+class Student{
+    int age;
+    String name;
+    String course;
+
+    void study(){
+        System.out.println("The student is studying...");
+    }
+
+    public class Main{
+        public static void main(String[] args) {
+            System.out.println("Student class created successfully");
+        }
+    }
+}
