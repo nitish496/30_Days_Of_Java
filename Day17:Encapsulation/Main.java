f0b19348d0d2545500e4c5ef4a61ce60b1a0a63c
T.NITISH <mbeastreacts471@gmail.com>
2026-07-25 22:40:26 +0530

    program-1 for day-17

---
+++ Day17:Encapsulation/Main.java
@@ -0,0 +1,14 @@
+//p-1
+class student{
+    private String name = "Nitish";
+    public void display(){
+        System.out.println("Name:"+name);
+    }
+}
+public class Main{
+    public static void main(String[]args){
+        student s1 = new student();
+        s1.display();
+        System.out.println();
+    }
+}
