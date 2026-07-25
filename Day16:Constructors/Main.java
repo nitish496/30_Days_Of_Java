d0294e8e77beb6d658756e455f95a674e0940608
T.NITISH <mbeastreacts471@gmail.com>
2026-07-25 13:02:21 +0530

    program-3 for day-16

--- Day16:Constructors/Main.java
+++ Day16:Constructors/Main.java
@@ -1,16 +1,59 @@
-//p-1
-class Student{
-    Student(){
-        System.out.println("constructor is called");
+// =========================================
+// Program 1 - Introduction to Constructor
+// =========================================
+
+class Student {
+
+    Student() {
+        System.out.println("Constructor is called.");
     }
-}
-//p-2:default constructor
-class teach{
-    //empty constructor
+
 }
 
-public class Main{
-    public static void main(String[] args) {
-        Student s1 = new Student();
+// =========================================
+// Program 2 - No-Argument Constructor
+// =========================================
+
+class Teacher {
+
+    // Java automatically provides a Default Constructor
+
+}
+//p-3:
+class StudentDetails {
+
+    String name;
+    int age;
+    String course;
+
+    StudentDetails() {
+
+        name = "Nitish";
+        age = 20;
+        course = "CSE-AI&ML";
+
     }
+
+    void display() {
+
+        System.out.println("Name   : " + name);
+        System.out.println("Age    : " + age);
+        System.out.println("Course : " + course);
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        StudentDetails s2 = new StudentDetails();
+
+        s2.display();
+
+    }
+
 }
