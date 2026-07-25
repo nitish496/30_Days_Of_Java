e1d1b307d09b3b1af744df9244e9ec36b5f18b19
T.NITISH <mbeastreacts471@gmail.com>
2026-07-25 12:48:48 +0530

    program-1 for day-16

---
+++ Day16:Constructors/Main.java
@@ -0,0 +1,12 @@
+//p-1
+class Student{
+    Student(){
+        System.out.println("constructor is called");
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
