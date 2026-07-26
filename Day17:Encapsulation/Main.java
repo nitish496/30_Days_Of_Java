6648ceed5239557b33bc15f8be94e3210de549ec
T.NITISH <mbeastreacts471@gmail.com>
2026-07-26 11:27:45 +0530

    program-2 for day-17

--- Day17:Encapsulation/Main.java
+++ Day17:Encapsulation/Main.java
@@ -5,10 +5,20 @@ class student{
         System.out.println("Name:"+name);
     }
 }
+
+
+
 public class Main{
     public static void main(String[]args){
         student s1 = new student();
         s1.display();
         System.out.println();
+        
+        Students s2 = new Students();
+        s2.displa();
+        System.out.println();
+
+   
     }
 }
+
