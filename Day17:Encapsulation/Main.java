edfcef5b14a1ca4c7f8c90dfc9e36d73674aba0c
T.NITISH <mbeastreacts471@gmail.com>
2026-07-26 11:29:14 +0530

    program-3 for day-17

--- Day17:Encapsulation/Main.java
+++ Day17:Encapsulation/Main.java
@@ -5,7 +5,34 @@ class student{
         System.out.println("Name:"+name);
     }
 }
+//p-2:
+class Students{
+ private String name = "Nitish";
+ private int age = 19;
 
+ public void displa(){
+    System.out.println("Name:"+name);
+    System.out.println("age:"+age);
+ }
+}
+//p-3:
+class stud{
+    private String name = "Raju";
+    private int age = 19;
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void Dsp(){
+        System.out.println("name:"+name);
+        System.out.println("age:"+age);
+        System.out.println("By using getter method");
+    }
+}
 
 
 public class Main{
@@ -17,7 +44,11 @@ public class Main{
         Students s2 = new Students();
         s2.displa();
         System.out.println();
-
+     
+        stud s3 = new stud();
+        s3.Dsp();
+        System.out.println();
+        stude s4 = new stude();
    
     }
 }
