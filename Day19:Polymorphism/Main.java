6d148ddf185347472544d9b5ffab3978a2e9d81e
T.NITISH <mbeastreacts471@gmail.com>
2026-07-28 09:19:10 +0530

    program-1 for day-19

--- Day19:Polymorphism/Main.java
+++ Day19:Polymorphism/Main.java
@@ -4,9 +4,16 @@ class Display{
         System.out.println("I wil be back.");
     }
 }
+
 public class Main{
     public static void main(String[] args) {
         Display d1 = new Display();
         d1.show();
+       //p-2:
+        Calculator c1 = new Calculator();
+        c1.add(10,20);
+        Calculator c2 = new Calculator();
+        c2.add(10,20,30);
     }
 }
+
