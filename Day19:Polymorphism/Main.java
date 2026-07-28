7517ca612fe116af050c51a4f76093615fbdd2c4
T.NITISH <mbeastreacts471@gmail.com>
2026-07-28 09:11:42 +0530

    program-1 for day-1

---
+++ Day19:Polymorphism/Main.java
@@ -0,0 +1,12 @@
+//p-1:
+class Display{
+    public void show(){
+        System.out.println("I wil be back.");
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        Display d1 = new Display();
+        d1.show();
+    }
+}
