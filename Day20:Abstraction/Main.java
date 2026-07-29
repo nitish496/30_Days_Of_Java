ca2313c5d7f9b3322cb0bc92fe8cf4336ac74102
T.NITISH <mbeastreacts471@gmail.com>
2026-07-29 09:01:02 +0530

    program-2 for day-20

--- Day20:Abstraction/Main.java
+++ Day20:Abstraction/Main.java
@@ -9,10 +9,30 @@ class Dog extends Animal{
     void sound(){
         System.out.println("Dog barks.");
     }
+
+}
+
+//p-2:
+abstract class Animal2{
+    void eat(){
+        System.out.println("Animal's eat food.");
+
+    }
+    abstract void sound();
+
+}
+class Dog2 extends Animal2{
+    @Override
+    void sound(){
+        System.out.println("Dog2 barks");
+    }
 }
 public class Main{
     public static void main(String[]args){
         Dog d1 = new Dog();
         d1.sound();
+        Dog2 d2 = new Dog2();
+        d2.eat();
+        d2.sound();
     }
 }
