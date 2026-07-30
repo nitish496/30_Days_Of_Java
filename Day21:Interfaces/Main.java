caba849f86860b6b8a20f9ad9074f9343e3e61e4
T.NITISH <mbeastreacts471@gmail.com>
2026-07-30 10:38:28 +0530

    program-2 for day-21

--- Day21:Interfaces/Main.java
+++ Day21:Interfaces/Main.java
@@ -9,9 +9,29 @@ class Laptop implements Switchable{
     }
 }
 
+interface Switchable2{
+    void turnOn();
+    void turnOff();
+
+}
+
+class Telvision implements Switchable2{
+    @Override
+    public void turnOn(){
+        System.out.println("tv has been turned on.");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("tv has been turned off.");
+    }
+}
 public class Main{
     public static void main(String[] args) {
         Laptop l1 = new Laptop();
         l1.turnOn();
+        System.out.println();
+        Telvision t1 = new Telvision();
+        t1.turnOn();
+        t1.turnOff();
     }
 }
