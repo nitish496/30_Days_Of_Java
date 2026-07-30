fa6f20d607c0a6529f5e7dd75464a792b9cf01aa
T.NITISH <mbeastreacts471@gmail.com>
2026-07-30 10:44:42 +0530

    program-3 for day-21

--- Day21:Interfaces/Main.java
+++ Day21:Interfaces/Main.java
@@ -14,8 +14,33 @@ interface Switchable2{
     void turnOff();
 
 }
-
+//p-2:
 class Telvision implements Switchable2{
+    @Override
+    public void turnOn(){
+        System.out.println("tv has been turned on.");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("tv has been turned off.");
+    }
+}
+//p-3:
+interface Switchable3{
+    void turnOn();
+    void turnOff();
+}
+class Laptop2 implements Switchable3{
+    @Override
+    public void turnOn(){
+        System.out.println("Laptop has been turned on.");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Laptop has been turned off.");
+    }
+}
+class Television2 implements Switchable3{
     @Override
     public void turnOn(){
         System.out.println("tv has been turned on.");
@@ -33,5 +58,13 @@ public class Main{
         Telvision t1 = new Telvision();
         t1.turnOn();
         t1.turnOff();
+        System.out.println();
+        Laptop2 l2 = new Laptop2();
+        l2.turnOn();
+        l2.turnOff();
+        Television2 t2 = new Television2();
+      t2.turnOn();
+      t2.turnOff();
+        System.out.println();
     }
 }
