56b82b9c70ea07f0f639690c51b1b75402acb941
T.NITISH <mbeastreacts471@gmail.com>
2026-07-31 10:01:00 +0530

    program-2 for day-22

--- Day22:Exception Handling/Main.java
+++ Day22:Exception Handling/Main.java
@@ -1,6 +1,7 @@
 //p-1:
 public class Main{
-    public static void main(String[] args) {
+    static void program1(){
+        System.out.println("program-1");
         try{
             int result = 10/0;
             System.out.println(result);
@@ -10,4 +11,22 @@ public class Main{
             System.out.println("Program ended.");
         }
     }
+    static void program2(){
+        System.out.println("Program-2");
+        try{
+            int[]arr={10,20,30};
+            System.out.println(arr[5]);
+        }catch(ArithmeticException e){
+            System.out.println("Arithmetic exception occurred"+e.getMessage());
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index out of bounds"+e.getMessage());
+        }finally{
+            System.out.println("Program ended.");
+        }
+    }
+ 
+ public static void main(String[]args){
+    program1();
+    program2();
+ }
 }
