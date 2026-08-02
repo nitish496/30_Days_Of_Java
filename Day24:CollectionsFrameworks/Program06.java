c529269038ab1400e26937c4e5d302ac187b674c
nitish496 <223207801+nitish496@users.noreply.github.com>
2026-08-02 23:16:44 +0530

    program-06 for day-24

---
+++ Day24:CollectionsFrameworks/Program06.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Program06 {
+
+    public static void main(String[] args) {
+
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("Red");
+        linkedHashSet.add("Green");
+        linkedHashSet.add("Blue");
+        System.out.println("LinkedHashSet: " + linkedHashSet);
+    }
+}
