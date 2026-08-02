0002cf41de195d7968949d2a3948deb9d487cdbd
nitish496 <223207801+nitish496@users.noreply.github.com>
2026-08-02 23:16:43 +0530

    program-01 for day-24

---
+++ Day24:CollectionsFrameworks/Program01.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Program01 {
+
+    public static void main(String[] args) {
+
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Java");
+        arrayList.add("Python");
+        arrayList.add("C++");
+        System.out.println("ArrayList: " + arrayList);
+    }
+}
