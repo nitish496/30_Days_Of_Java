b50b8af807cafe1d21aabc23e995c4ec862b2f56
nitish496 <223207801+nitish496@users.noreply.github.com>
2026-08-02 23:16:44 +0530

    program-07 for day-24

---
+++ Day24:CollectionsFrameworks/Program07.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Program07 {
+
+    public static void main(String[] args) {
+
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(50);
+        treeSet.add(10);
+        treeSet.add(30);
+        System.out.println("TreeSet (sorted): " + treeSet);
+    }
+}
