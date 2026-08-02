0143189f2bb89396a9363918eb401ccbb28f777c
nitish496 <223207801+nitish496@users.noreply.github.com>
2026-08-02 22:56:15 +0530

    program-5 for day-24

---
+++ Day24:CollectionsFrameworks/TreeSetDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(50);
+        treeSet.add(10);
+        treeSet.add(30);
+        System.out.println("TreeSet: " + treeSet);
+    }
+}
