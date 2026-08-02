88d27d7c2622c21954df73b8d38e03b5372e9c91
nitish496 <223207801+nitish496@users.noreply.github.com>
2026-08-02 23:16:43 +0530

    program-03 for day-24

---
+++ Day24:CollectionsFrameworks/Program03.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Program03 {
+
+    public static void main(String[] args) {
+
+        List<Integer> vector = new Vector<>();
+        vector.add(10);
+        vector.add(20);
+        System.out.println("Vector: " + vector);
+    }
+}
